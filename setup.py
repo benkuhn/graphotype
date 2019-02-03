e62f9f0eccd251d2cdda9fc2f0572ee2e64dbc6f
Lincoln Quirk <lincolnq@gmail.com>
2019-02-03 17:31:56 +0000

    fix setup.py:
    - correctly installs graphotype and sub-package
    - correctly installs subpackage templates

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name="graphotype",
     python_requires='>=3.6.0',
-    py_modules=['graphotype'],
+    packages=['graphotype', 'graphotype.import_schema'],
+    package_data={'graphotype.import_schema': ['templates/*.py.tmpl']},
     version='0.0.1',
     install_requires=[
         'graphql-core>=2.1',
