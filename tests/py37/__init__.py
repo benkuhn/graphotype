8625efec25ad2111e1c4bda8a8383be23adf8eb4
Ben Kuhn <ben.s.kuhn@gmail.com>
2019-02-03 14:53:06 +0000

    fix 3.7

---
+++ tests/py37/__init__.py
@@ -0,0 +1,7 @@
+"""py37 -- the directory for tests that only run on py37 or later.
+
+We configure this in `tests/conftest.py`.
+
+The __init__ file here is necessary to avoid test name collisions with test
+files in the parent dir.
+"""
